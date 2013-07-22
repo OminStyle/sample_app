2ca0af41cac590bb1cd7c01ce2e103c07e5259ff
OminStyle <name.cmh@gmail.com>
2013-07-21 21:38:24 -0700

    updated layouts footer

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,6 +3,7 @@ module UsersHelper
   def gravatar_for(user)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatars/#{gravatar_id}.png"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    #image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    image_tag(gravatar_url, class: "gravatar")
   end
 end
